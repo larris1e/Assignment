b0f183b14a6bbf4bf113c6f12cab5eb158897505
Sammydepoj <samueladepoju86@yahoo.com>
2022-10-15 08:36:47 +0100

    user registration handled

--- php/login.php
+++ php/login.php
@@ -1,13 +1,16 @@
 <?php
 if(isset($_POST['submit'])){
-    $username = //finish this line
-    $password = //finish this
+    $username = $_POST['email']; //finish this line
+    $password = $_POST['password'];//finish this
 
 loginUser($email, $password);
 
 }
 
 function loginUser($email, $password){
+
+
+    
     /*
         Finish this function to check if username and password 
     from file match that which is passed from the form
--- php/register.php
+++ php/register.php
@@ -10,9 +10,21 @@ registerUser($username, $email, $password);
 
 function registerUser($username, $email, $password){
     //save data into the file
-    
+    $data=array($username,$email,$password."</br>");
+    $file = fopen('../storage/users.csv','a');
+
+    if($file){
+        // echo "file opened"."<br>" ."and"."<br>";
+        fputcsv($file,$data);
+        fclose($file);
+        echo "<script> alert ('User Successfully Registered'); </script>";
+        header("location:../forms/login.html");
+    }
+    else{
+        echo "<script> alert ('Not Registered'); </script>";
+    }   
     // echo "OKAY";
 }
-echo "HANDLE THIS PAGE";
+// echo "HANDLE THIS PAGE";
 
 
