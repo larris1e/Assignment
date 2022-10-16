375796695b11150e043df1e530f22049420e8f3c
Sammydepoj <samueladepoju86@yahoo.com>
2022-10-16 09:36:17 +0100

    login functionality in progress

--- php/login.php
+++ php/login.php
@@ -2,20 +2,35 @@
 if(isset($_POST['submit'])){
     $username = $_POST['email']; //finish this line
     $password = $_POST['password'];//finish this
-
+    echo "$username.$password";
 loginUser($email, $password);
-
 }
-
 function loginUser($email, $password){
-
-
-    
+    $file = fopen('../storage/users.csv','ra+');
+    if($file){
+        while ($getdata=fgetcsv($file)){
+            echo" $getdata[0].$getdata[1].$getdata[2]";
+            if ($getdata[1]== $email && $getdata[2]==$password){
+                echo "
+                <script>
+                alert ('You have succesfully logged in');
+                // window.location.href='dashboard.php';
+                </script>";
+            }
+            else{
+                echo "
+                <script>
+                 alert ('Incorrect credentials!');
+                //  window.location.href='../forms/login.html';
+                </script>";
+            }
+            fclose($file);
+        }        
+    }
     /*
         Finish this function to check if username and password 
     from file match that which is passed from the form
     */
 }
-
 echo "HANDLE THIS PAGE";
 
--- php/register.php
+++ php/register.php
@@ -17,14 +17,26 @@ function registerUser($username, $email, $password){
         // echo "file opened"."<br>" ."and"."<br>";
         fputcsv($file,$data);
         fclose($file);
-        echo "<script> alert ('User Successfully Registered'); </script>";
-        header("location:../forms/login.html");
+        echo "
+        <script>
+         alert ('User Successfully Registered! Proceed to login to your account now');
+         window.location.href='../forms/login.html';
+        </script>";
     }
     else{
-        echo "<script> alert ('Not Registered'); </script>";
+        echo "
+        <script>
+         alert ('Not Registered');
+         window.location.href='../forms/register.html';
+        </script>";
     }   
     // echo "OKAY";
 }
 // echo "HANDLE THIS PAGE";
+    // echo "
+    // <script>
+    //  alert ('No input from user found');
+    //  window.location.href='../forms/register.html';
+    // </script>";
 
 
