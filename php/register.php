8e4713c20ac9fa35b10195635539350387588835
Sammydepoj <samueladepoju86@yahoo.com>
2022-10-19 09:48:22 +0100

    reset password

--- php/register.php
+++ php/register.php
@@ -11,29 +11,39 @@ registerUser($username, $email, $password);
 
 function registerUser($username, $email, $password){
 
-    //save data into the file
-    $data=array($username,$email,$password);
-    $file = fopen('../storage/users.csv','a');
-
-    $readFile =fopen('../storage/users.csv','r');
-    $checkFile=fgetcsv($readFile);
-
-    if($checkFile[1] == $email){
-        echo "
-        <script>
-         alert ('This email has been used already');
-         window.location.href='../forms/register.html';
-        </script>";
+    function checkUser($email){
+        $checkFile = fopen('../storage/users.csv', 'r');
+        while(!feof($checkFile)){
+           $getUser = fgetcsv($checkFile); 
+            if($getUser[1] == $email){
+                return true;
+            }
+        }
+        fclose($checkFile);
+        return false;
     }
-    else{
-        fputcsv($file,$data);
+
+    
+    $data=array($username,$email,$password);
+
+    $file = fopen('../storage/users.csv','a');
+    $user= checkUser($email);
+
+    if($user){
+    echo "
+    <script>
+     alert ('This email has been used already');
+     window.location.href='../forms/register.html';
+    </script>"; 
+   }
+else{
+        $file = fopen('../storage/users.csv', 'a');
+        fputcsv($file, $data);
         fclose($file);
-        echo "
-        <script>
-         alert ('User Successfully Registered! Proceed to login to your account now');
-         window.location.href='../forms/login.html';
-        </script>";
-    }   
+    echo "
+    <script>
+     alert ('User Successfully Registered! Proceed to login to your account now');
+     window.location.href='../forms/login.html';
+    </script>";
+    }
 }
-
-
--- php/reset.php
+++ php/reset.php
@@ -11,22 +11,26 @@ function resetPassword($email, $newpassword){
         //if it does, replace the password
 
     $readFile =fopen('../storage/users.csv','r');
-    $checkFile=fgetcsv($readFile);
 
-    if ($checkFile[1] == $email){
-        $openFile = fopen('../storage/users.csv','a');
-        $datas=[$checkFile[0],$email,$newpassword];
-        fputcsv($openFile,$datas);
-        
-        echo" <script> alert('Password Successfully modified!');
-         window.location.href='../forms/login.html';
-        </script>";
-        fclosecsv($openFile);
-        }
-    else{
-        echo" <script> alert('User does not exit');
-         window.location.href='../forms/resetpassword.html';
-        </script>";
-    }  
+    while(!feof($readFile)){
+        $checkFile=fgetcsv($readFile);
+        if ($checkFile[1] == $email){
+            
+            $checkFile[2] = $newpassword;
+            $openFile = fopen('../storage/users.csv','w');
+            fputcsv($openFile,$checkFile);
+            
+            echo" <script> alert('Password Successfully modified!');
+             window.location.href='../forms/login.html';
+            </script>";
+            fclosecsv($openFile);
+            }
+        else{
+            echo" <script> alert('User does not exit');
+             window.location.href='../forms/resetpassword.html';
+            </script>";
+        } 
+    }
+     
 }
 
